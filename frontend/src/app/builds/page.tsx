508c819e88498596696e5d4e0431e917f3bd75fb
Umair <uandrabi@voceracommunications.com>
2023-12-19 15:22:32 +0530

    Remove builds page

--- frontend/src/app/builds/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function BuildsPage() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-4xl font-bold">Builds</h1>
-    </div>
-  );
-};
